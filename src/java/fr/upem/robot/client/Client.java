3df313afa3921c68cc70ea95578c86895efe76d1
Michael Remy <remy.mick@gmail.com>
2014-03-21 17:56:22 +0100

    Modif client/server without any NamingServices (bugs)

--- src/java/fr/upem/robot/client/Client.java
+++ src/java/fr/upem/robot/client/Client.java
@@ -1,40 +1,53 @@
 package fr.upem.robot.client;
 
+import fr.upem.robot.*;
+import org.omg.CORBA.Any;
 import org.omg.CORBA.ORB;
-import org.omg.CosNaming.NameComponent;
-import org.omg.CosNaming.NamingContextExt;
-import org.omg.CosNaming.NamingContextExtHelper;
-
-import fr.upem.robot.AlreadyStarted;
-import fr.upem.robot.InvalidPosition;
-import fr.upem.robot.Position;
-import fr.upem.robot.RobotPilote;
-import fr.upem.robot.RobotPiloteHelper;
+import org.omg.CORBA.TypeCode;
 
 public class Client {
 
-	public static void main(String[] args) throws Exception {
-		ORB orb = ORB.init(args, null);
+    public static void main(String[] args) throws Exception {
+        ORB orb = ORB.init(args, null);
 
-		NamingContextExt namingContext = NamingContextExtHelper.narrow(orb
-				.resolve_initial_references("NameService"));
+        String IORRobotPilote = args[0];
+        String IORRobotControl = args[1];
 
-		NameComponent[] nameComponentsRobotPilote = namingContext
-				.to_name("RobotContext/RobotPilote");
+        org.omg.CORBA.Object refRobotPilote = orb.string_to_object(IORRobotPilote);
+        org.omg.CORBA.Object refRobotControle = orb.string_to_object(IORRobotControl);
 
-		RobotPilote robotPilote = RobotPiloteHelper.narrow(namingContext
-				.resolve(nameComponentsRobotPilote));
-		try {
-			robotPilote.move(new Position(0, -1));
-		} catch (InvalidPosition e) {
-			System.out.println(e.message);
-		}
+        RobotPilote robotPilote = RobotPiloteHelper.narrow(refRobotPilote);
+        RobotControl robotControl = RobotControlHelper.narrow(refRobotControle);
 
-		try {
-			robotPilote.start();
-		} catch (AlreadyStarted e) {
-			System.out.println(e.message);
-		}
-	}
+        Any anySensor = orb.create_any();
+        SensorA sensorA = new SensorA("sensor A1");
+        SensorAHelper.insert(anySensor, sensorA);
+        robotControl.setSensor("sensor A1", anySensor);
+
+        anySensor = robotControl.getSensor("sensor A1");
+        TypeCode tc = anySensor.type();
+        if (tc.equal(SensorAHelper.type())) {
+            SensorA tmp = SensorAHelper.extract(anySensor);
+            System.out.println("Name : " + tmp.name);
+        } else if (tc.equal(SensorBHelper.type())) {
+            SensorB tmp = SensorBHelper.extract(anySensor);
+            System.out.println("Name : " + tmp.name);
+        } else if (tc.equal(SensorCHelper.type())) {
+            SensorC tmp = SensorCHelper.extract(anySensor);
+            System.out.println("Name : " + tmp.name);
+        }
+
+        try {
+            robotPilote.move(new Position(0, -1));
+        } catch (InvalidPosition e) {
+            System.out.println(e.message);
+        }
+
+        try {
+            robotPilote.start();
+        } catch (AlreadyStarted e) {
+            System.out.println(e.message);
+        }
+    }
 
 }
--- src/java/fr/upem/robot/server/Server.java
+++ src/java/fr/upem/robot/server/Server.java
@@ -1,5 +1,7 @@
 package fr.upem.robot.server;
 
+import fr.upem.robot.RobotControlPOATie;
+import fr.upem.robot.servant.RobotControlImpl;
 import org.omg.CORBA.ORB;
 import org.omg.PortableServer.POA;
 import org.omg.PortableServer.POAHelper;
@@ -15,14 +17,24 @@ public class Server {
 				.resolve_initial_references("RootPOA"));
 
 		RobotPiloteImpl robotPilote = new RobotPiloteImpl();
+        RobotControlImpl robotControl = new RobotControlImpl();
+
 		RobotPilotePOATie robotPilotePOATie = new RobotPilotePOATie(robotPilote);
+        RobotControlPOATie robotControlPOATie = new RobotControlPOATie(robotControl);
 
-		byte[] servantIdRobotPilote = rootPOA
-				.activate_object(robotPilotePOATie);
+        byte[] servantIdRobotPilote = rootPOA
+                .activate_object(robotPilotePOATie);
+        byte[] servantIdRobotControl = rootPOA
+                .activate_object(robotControlPOATie);
 
-		org.omg.CORBA.Object ref = rootPOA.id_to_reference(servantIdRobotPilote);
-		String ior = orb.object_to_string(ref);
-		System.out.println(ior);
+		org.omg.CORBA.Object refRobotPilote = rootPOA.id_to_reference(servantIdRobotPilote);
+		org.omg.CORBA.Object refRobotControl = rootPOA.id_to_reference(servantIdRobotControl);
+
+		String iorRobotPilote = orb.object_to_string(refRobotPilote);
+		String iorRobotControl = orb.object_to_string(refRobotControl);
+
+		System.out.println("RobotPilote : " + iorRobotPilote);
+		System.out.println("RobotControl : " + iorRobotControl);
 
 		rootPOA.the_POAManager().activate();
 		orb.run();
