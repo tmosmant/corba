4278abc81013c4601a15c8cd7afee735cc78af61
Michael Remy <remy.mick@gmail.com>
2014-03-25 19:03:08 +0100

    Modif client (python) with namming service

--- src/python/fr/upem/robot/client/client.py
+++ src/python/fr/upem/robot/client/client.py
@@ -1,66 +1,71 @@
-import sys
+#!/usr/bin/env python
 
+import sys
+import CosNaming
 from omniORB import CORBA, any
 from fr.upem.robot import *
 
-orb = CORBA.ORB_init(sys.argv, CORBA.ORB_ID)
+nsref = open(sys.argv[1], 'r')
+ior = "NameService=" + nsref.read()
+orb = CORBA.ORB_init(["-ORBInitRef", ior], CORBA.ORB_ID)
 
-iorRobotPilote = sys.argv[1]
-iorRobotControl = sys.argv[2]
+ref = orb.resolve_initial_references("NameService");
+namingContext = ref._narrow(CosNaming.NamingContext)
 
-refRobotPilote = orb.string_to_object(iorRobotPilote)
-refRobotControl = orb.string_to_object(iorRobotControl)
+nameRobotPilote = namingContext.to_name("RobotContext/RobotPilote")
+nameRobotControl = namingContext.to_name("RobotContext/RobotControl")
 
-robotPilote = refRobotPilote._narrow(RobotPilote)
-robotControl = refRobotControl._narrow(RobotControl)
+objRobotPilote = namingContext.resolve(nameRobotPilote)
+objRobotControl = namingContext.resolve(nameRobotControl)
+
+robotPilote = objRobotPilote._narrow(namingContext.resolve(nameRobotPilote))
+robotControl = objRobotControl._narrow(namingContext.resolve(nameRobotControl))
 
 if robotPilote is None:
-    print "Object reference is not an Example::Echo"
-    sys.exit(1)
-
+	print "Object reference is not an Example::Echo"
+	sys.exit(1)
 if robotControl is None:
-    print "Object reference is not an Example::Echo"
-    sys.exit(1)
-
-#try:
-#	eo.start()
-#except:
-#	print "Le robot a deja demarre"
-
-pos = Position(3,2)
-try:
-    robotPilote.move(pos)
-except:
-    print "Mouvement non valide"
+	print "Object reference is not an Example::Echo"
+	sys.exit(1)
 
 try:
-    result = robotPilote.stop()
-    print result.posX
-    print result.posY
+	robotPilote.start()
 except:
-    print "Le robot est deja a l'arret"
+	print "Le robot a deja demarre"
 
+pos = Position(3, 1)
 try:
-    robotPilote.start()
+	robotPilote.move(pos)
 except:
-    print "Le robot a deja demarre"
-
+	print "Mouvement non valide"
+try:
+	result = robotPilote.stop()
+	print result.posX
+	print result.posY
+except:
+	print "Le robot est deja a l'arret"
+	
+try:
+	robotPilote.start()
+except:
+	print "Le robot a deja demarre"
+	
 pos = Position(8,5)
 try:
-    robotPilote.move(pos)
+	robotPilote.move(pos)
 except:
-    print "Mouvement non valide"
+	print "Mouvement non valide"
 
 try:
-    result = robotPilote.stop()
-    print result.posX
-    print result.posY
+	result = robotPilote.stop()
+	print result.posX
+	print result.posY
 except:
-    print "Le robot est deja a l'arret"
+	print "Le robot est deja a l'arret"
 
 sensor = SensorB("Sensor B1")
 anySensor = any.to_any(sensor)
-#robotControl.setSensor(sensor.name, anySensor)
+robotControl.setSensor(sensor.name, anySensor)
 a = robotControl.getSensor(sensor.name)
 typecode = a.typecode()
 v = a.value(typecode)
