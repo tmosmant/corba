f5f83ac2911ce03feb687123c9c400303849fdbc
Michael Remy <remy.mick@gmail.com>
2014-03-25 19:00:19 +0100

    Modif server (java) with namming service

--- src/java/fr/upem/robot/server/Server.java
+++ src/java/fr/upem/robot/server/Server.java
@@ -1,43 +1,54 @@
 package fr.upem.robot.server;
 
 import fr.upem.robot.RobotControlPOATie;
+import fr.upem.robot.RobotPilotePOATie;
 import fr.upem.robot.servant.RobotControlImpl;
+import fr.upem.robot.servant.RobotPiloteImpl;
 import org.omg.CORBA.ORB;
+import org.omg.CosNaming.NameComponent;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+import org.omg.CosNaming.NamingContextPackage.AlreadyBound;
 import org.omg.PortableServer.POA;
 import org.omg.PortableServer.POAHelper;
 
-import fr.upem.robot.RobotPilotePOATie;
-import fr.upem.robot.servant.RobotPiloteImpl;
-
 public class Server {
 
-	public static void main(String[] args) throws Exception {
-		ORB orb = ORB.init(args, null);
-		POA rootPOA = POAHelper.narrow(orb
-				.resolve_initial_references("RootPOA"));
+    public static void main(String[] args) throws Exception {
+        ORB orb = ORB.init(args, null);
+        POA rootPOA = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
 
-		RobotPiloteImpl robotPilote = new RobotPiloteImpl();
+        RobotPiloteImpl robotPilote = new RobotPiloteImpl();
         RobotControlImpl robotControl = new RobotControlImpl();
 
-		RobotPilotePOATie robotPilotePOATie = new RobotPilotePOATie(robotPilote);
+        RobotPilotePOATie robotPilotePOATie = new RobotPilotePOATie(robotPilote);
         RobotControlPOATie robotControlPOATie = new RobotControlPOATie(robotControl);
 
-        byte[] servantIdRobotPilote = rootPOA
-                .activate_object(robotPilotePOATie);
-        byte[] servantIdRobotControl = rootPOA
-                .activate_object(robotControlPOATie);
+        byte[] servantIdRobotPilote = rootPOA.activate_object(robotPilotePOATie);
+        byte[] servntIdRobotControl = rootPOA.activate_object(robotControlPOATie);
 
-		org.omg.CORBA.Object refRobotPilote = rootPOA.id_to_reference(servantIdRobotPilote);
-		org.omg.CORBA.Object refRobotControl = rootPOA.id_to_reference(servantIdRobotControl);
+        NamingContextExt context = NamingContextExtHelper.narrow(orb.resolve_initial_references("NameService"));
+        try {
+            context.bind_new_context(context.to_name("RobotContext"));
+        } catch (AlreadyBound e) {
+            context.rebind_context(context.to_name("RobotContext"), context);
+        }
 
-		String iorRobotPilote = orb.object_to_string(refRobotPilote);
-		String iorRobotControl = orb.object_to_string(refRobotControl);
+        NameComponent[] nameRobotPilote = context.to_name("RobotContext/RobotPilote");
+        try {
+            context.bind(nameRobotPilote, rootPOA.id_to_reference(servantIdRobotPilote));
+        } catch (AlreadyBound e) {
+            context.rebind(nameRobotPilote, rootPOA.id_to_reference(servantIdRobotPilote));
+        }
+        NameComponent[] nameRobotControl = context.to_name("RobotContext/RobotControl");
+        try {
+            context.bind(nameRobotControl, rootPOA.id_to_reference(servntIdRobotControl));
+        } catch (AlreadyBound e) {
+            context.rebind(nameRobotControl, rootPOA.id_to_reference(servntIdRobotControl));
+        }
 
-		System.out.println("RobotPilote : " + iorRobotPilote);
-		System.out.println("RobotControl : " + iorRobotControl);
-
-		rootPOA.the_POAManager().activate();
-		orb.run();
-	}
+        rootPOA.the_POAManager().activate();
+        orb.run();
+    }
 
 }
